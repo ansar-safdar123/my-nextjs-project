1d4ef3c2b8596f61769778b32a55402491fb46d3
ansar-safdar123 <ansarsafdar143@gmail.com>
2025-10-17 01:19:54 +0500

    fix

--- app/page.js
+++ app/page.js
@@ -91,7 +91,9 @@ export default function Home() {
     <div className="">
       <div className="home">
         <div className="text-white px-3 sm:px-7 md:px-20 ">
-          <h1 className="text-center VCR-font-family">WELCOME TO OPENTHATPACK</h1>
+          <h1 className="text-center VCR-font-family">
+            WELCOME TO OPENTHATPACK
+          </h1>
 
           <CircularMenu
             onMenuChange={handleMenuChange}
@@ -165,20 +167,21 @@ export default function Home() {
 
       {/* Sidebar */}
       <div
-        className={`fixed top-0 right-0 h-full w-[300px] md:w-[375px] font-exo bg-[#EBEBEB] z-50 shadow-lg transform transition-transform duration-500 ${isOpen ? "translate-x-0" : "translate-x-full"
-          }`}
+        className={`fixed top-0 right-0 h-full w-[300px] md:w-[375px] font-exo bg-[#EBEBEB] z-50 shadow-lg transform transition-transform duration-500 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
       >
         {/* Close Button */}
         <button
           onClick={() => setIsOpen(false)}
-          className="absolute top-4 right-4 text-2xl"
+          className="absolute top-4 z-50 right-4 text-2xl"
         >
           ✕
         </button>
 
         {/* Sidebar Content */}
-        <div className="p-6 relative flex flex-col font-normal md:gap-6 gap-3 text-[#4E46CB] text-lg md:text-2xl">
-          <div className="w-[120px] h-[60px] md:w-[141px] md:h-[87px] relative my-4">
+        <div className="p-6 font-teko relative flex flex-col font-normal md:gap-4 gap-3 text-[#4E46CB] text-lg md:text-2xl">
+          <div className="w-[120px]  md:w-[141px] md:h-[87px] relative my-4">
             <Image
               src="/images/sideMenuBar/logo1.png"
               className="object-contain"
@@ -186,12 +189,42 @@ export default function Home() {
               fill
             />
           </div>
-          <a href="#">SHOP</a>
-          <a href="#">MARKETPLACE</a>
-          <a href="#">HOW IT WORKS</a>
-          <a href="#">QUALITY</a>
-          <a href="#">FAIRNESS</a>
-          <a href="#">FAQS</a>
+          <a
+            href="#"
+            class="px-3 rounded-lg text-[#4E46CB] hover:bg-[#4E46CB] transition hover:text-white duration-300"
+          >
+            SHOP
+          </a>
+          <a
+            href="#"
+            class="px-3 rounded-lg text-[#4E46CB] hover:bg-[#4E46CB] transition hover:text-white duration-300"
+          >
+            MARKETPLACE
+          </a>
+          <a
+            href="#"
+            class="px-3 rounded-lg text-[#4E46CB] hover:bg-[#4E46CB] transition hover:text-white duration-300"
+          >
+            HOW IT WORKS
+          </a>
+          <a
+            href="#"
+            class="px-3 rounded-lg text-[#4E46CB] hover:bg-[#4E46CB] transition hover:text-white duration-300"
+          >
+            QUALITY
+          </a>
+          <a
+            href="#"
+            class="px-3 rounded-lg text-[#4E46CB] hover:bg-[#4E46CB] transition hover:text-white duration-300"
+          >
+            FAIRNESS
+          </a>
+          <a
+            href="#"
+            class="px-3 rounded-lg text-[#4E46CB] hover:bg-[#4E46CB] transition hover:text-white duration-300"
+          >
+            FAQS
+          </a>
         </div>
         <div className="flex md:absolute bottom-10 items-center mt-5 gap-5 px-6">
           <button className="border text-sm border-[#4E46CB] px-3 md:px-8 py-2 text-[#4E46CB] rounded-full ">
@@ -199,17 +232,17 @@ export default function Home() {
           </button>
           <div className="flex items-center justify-center bg-gray-900">
             <button
-              className="relative w-10 h-10 md:w-16 md:h-16 border-2 border-white overflow-hidden group"
+              className="sticky-button relative w-10 h-10 md:w-12 md:h-[50px] border-2 border-white overflow-hidden group "
               onClick={() => setIsOpen(false)}
             >
-              {/* Hover background that slides up from bottom */}
+              {/* Hover background that slides up */}
               <div className="absolute inset-0 bg-white translate-y-full group-hover:translate-y-0 transition-transform duration-500 ease-in-out"></div>
 
               {/* Menu lines */}
-              <div className="relative z-10 flex flex-col items-center justify-center h-full space-y-1 md:space-y-2.5">
-                <div className="w-6 md:w-10 h-0.5 bg-white group-hover:bg-gray-900 transition-colors duration-500"></div>
-                <div className="w-6 md:w-10 h-0.5 bg-white group-hover:bg-gray-900 transition-colors duration-500"></div>
-                <div className="w-6 md:w-10 h-0.5 bg-white group-hover:bg-gray-900 transition-colors duration-500"></div>
+              <div className="relative flex flex-col items-center justify-center h-full space-y-1 md:space-y-2">
+                <div className="w-5 md:w-7 h-0.5 bg-white group-hover:bg-gray-900 transition-colors duration-500"></div>
+                <div className="w-5 md:w-7 h-0.5 bg-white group-hover:bg-gray-900 transition-colors duration-500"></div>
+                <div className="w-5 md:w-7 h-0.5 bg-white group-hover:bg-gray-900 transition-colors duration-500"></div>
               </div>
             </button>
           </div>
